756cb4d98c09f74c98fdda36bef99bbce9adff92
Sidareus <sidareus@gmail.com>
2011-04-28 14:57:57 -0700

    toms new menu manager

--- SSORFwindows/SSORFwindows/Management/MenuManager.cs
+++ SSORFwindows/SSORFwindows/Management/MenuManager.cs
@@ -14,12 +14,23 @@ using Microsoft.Xna.Framework.Media;
 
 namespace SSORF.Management
 {
+    
     class MenuManager
     {
-        //Needs a lot more submenus.
-        States.SubMenu MissionsMenu;
+
         //Which menu is currently being viewed?
-        States.SubMenu CurrentMenu;
+        enum Menu
+        {
+            Main,
+            VehicleSelect,
+            Dealership,
+            Missions,
+            NumMenus
+        }
+        Menu CurrentMenu;
+        //Needs a lot more submenus.
+        States.SubMenu[] Menus;
+        
         //each menu has the same cursor image so i made it a member of MenuManager
         private Texture2D CursorImage;
 
@@ -30,43 +41,62 @@ namespace SSORF.Management
 
         public MenuManager(ContentManager content)
         {
+            Menus = new States.SubMenu[(int)Menu.NumMenus];
             //load missionsMenu stuff
-            MissionsMenu = new States.SubMenu(2); //mission menu has 2 buttons
-            MissionsMenu.BackGround = content.Load<Texture2D>("Textures\\menu");
-            MissionsMenu.ButtonImage[0] = content.Load<Texture2D>("Textures\\button1");
-            MissionsMenu.ButtonPosition[0] = new Vector2(100, 300);
-            MissionsMenu.ButtonImage[1] = content.Load<Texture2D>("Textures\\button2");
-            MissionsMenu.ButtonPosition[1] = new Vector2(450, 300);
+            Menus[(int)Menu.Missions] = new States.SubMenu(2); //mission menu has 2 buttons
+            Menus[(int)Menu.Missions].BackGround = content.Load<Texture2D>("Images\\menu");
+            Menus[(int)Menu.Missions].ButtonImage[0] = content.Load<Texture2D>("Images\\button1");
+            Menus[(int)Menu.Missions].ButtonPosition[0] = new Vector2(100, 300);
+            Menus[(int)Menu.Missions].ButtonImage[1] = content.Load<Texture2D>("Images\\button2");
+            Menus[(int)Menu.Missions].ButtonPosition[1] = new Vector2(450, 300);
             
             //load cursor image and set current menu to missions menu
-            CursorImage = content.Load<Texture2D>("Textures\\cursor");
-            CurrentMenu = MissionsMenu;
+            CursorImage = content.Load<Texture2D>("Images\\cursor");
+
+            //This should be changed to main menu once we have it working
+            CurrentMenu = Menu.Missions;
+            
         }
 
         public void update(GameTime gameTime)
         {
-            
-            CurrentMenu.update(gameTime);
-            //After updating the menu we could check to see 
-            //if we need to return to a previous menu or proceed 
-            //to the next menu and set the currentMenu to something else;
 
-            //If we are in the missions menu and a button is pressed...
-            if (CurrentMenu == MissionsMenu && CurrentMenu.buttonPressed != 0)
-            {
-                //Set selected mission to a value other than zero to deactivate the menu
-                selectedMission = CurrentMenu.buttonPressed;
-                //reset buttonPressed for when we return to the menu
-                CurrentMenu.buttonPressed = 0;
+            Menus[(int)CurrentMenu].update(gameTime);
+
+            //Here we could add logic to switch current menu if the back button is pressed, or return to main menu
+            switch (CurrentMenu)
+            { 
+                case Menu.Missions :
+
+                    //Note: buttonPressed = 0 means no button has been pressed
+                    //If we are in the missions menu and a button is pressed...
+                    if (Menus[(int)Menu.Missions].buttonPressed != 0)
+                    {
+                        //Set selected mission to a value other than zero to deactivate the menu
+                        selectedMission = Menus[(int)Menu.Missions].buttonPressed;
+                        //reset buttonPressed for when we return to the menu
+                        Menus[(int)Menu.Missions].buttonPressed = 0;
+                    }
+
+                    
+                    break;
+
+                case Menu.VehicleSelect :
+                    //Code goes here
+                    break;
+
+                    //etc.
             }
+            
+            
         }
 
         //draw the current menu and the cursor
         public void draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
-            CurrentMenu.draw(spriteBatch);
-            spriteBatch.Draw(CursorImage, CurrentMenu.CursorPosition, Color.White);
+            Menus[(int)CurrentMenu].draw(spriteBatch);
+            spriteBatch.Draw(CursorImage, Menus[(int)CurrentMenu].CursorPosition, Color.White);
             spriteBatch.End();
         }
 
