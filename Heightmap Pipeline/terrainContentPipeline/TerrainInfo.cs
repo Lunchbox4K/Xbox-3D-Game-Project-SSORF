384167dafeb0470a1b5578eb9df7d241f48f8347
Administrator <Administrator@SNH219-14.acs.edcc.ctc.edu>
2011-05-03 16:27:14 -0700

    New menu crap

---
+++ Heightmap Pipeline/terrainContentPipeline/TerrainInfo.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+using Microsoft.Xna.Framework;
+
+namespace _3dOnlineGamePipeline
+{
+    public class TerrainInfoContent
+    {
+        #region Members
+
+        public float[,] Height
+        { get { return height; } }
+        private float[,] height;
+
+        public Vector3[,] Normals
+        { get { return normals; } set { normals = value; } }
+        private Vector3[,] normals;
+
+        public float TerrainScale
+        { get { return terrainScale; } }
+        private float terrainScale;
+
+        #endregion
+
+        public TerrainInfoContent(MeshContent terrainMesh, float TerrainScale, 
+            int terrainWidth, int terrainLength)
+        {
+            // validate input params
+            if (terrainMesh == null)
+                throw new ArgumentNullException("terrainMesh");
+            if (terrainWidth <= 0)
+                throw new ArgumentOutOfRangeException("terrainWidth");
+            if (terrainLength <= 0)
+                throw new ArgumentOutOfRangeException("terrainLength");
+
+            terrainScale = TerrainScale;
+
+            height = new float[terrainWidth, terrainLength];
+            normals = new Vector3[terrainWidth, terrainLength];
+
+            //Look at terrain mesh
+            GeometryContent geometry = terrainMesh.Geometry[0];
+            for (int i = 0; i < geometry.Vertices.VertexCount; i++)
+            {
+                // ... and look up its position and normal.
+                Vector3 location = geometry.Vertices.Positions[i];
+                Vector3 normal = (Vector3)geometry.Vertices.Channels
+                    [VertexChannelNames.Normal()][i];
+
+                // from the position's X and Z value, we can tell what X and Y
+                // coordinate of the arrays to put the height and normal into.
+                int arrayX = (int)
+                    ((location.X / terrainScale) + (terrainWidth - 1) / 2.0f);
+                int arrayY = (int)
+                    ((location.Z / terrainScale) + (terrainLength - 1) / 2.0f);
+
+                height[arrayX, arrayY] = location.Y;
+                normals[arrayX, arrayY] = normal;
+            }
+        }
+    }
+
+    [ContentTypeWriter]
+    public class TerrainInfoWriter : ContentTypeWriter<TerrainInfoContent>
+    {
+        protected override void Write(ContentWriter output, TerrainInfoContent value)
+        {
+            output.Write(value.TerrainScale);
+
+            output.Write(value.Height.GetLength(0));
+            output.Write(value.Height.GetLength(1));
+            foreach (float height in value.Height)
+            {
+                output.Write(height);
+            }
+            foreach (Vector3 normal in value.Normals)
+            {
+                output.Write(normal);
+            }
+        }
+        public override string GetRuntimeType(TargetPlatform targetPlatform)
+        {
+            return "_3dOnlineGame.TerrainInfo, " +
+                "3DGame2, Version=1.0.0.0, Culture=neutral";
+        }
+
+        public override string GetRuntimeReader(TargetPlatform targetPlatform)
+        {
+            return "_3dOnlineGame.TerrainInfoReader, " +
+                "3DGame2, Version=1.0.0.0, Culture=neutral";
+        }
+    }
+}
---
+++ SSORFwindows/SSORFwindows/Management/States/Mission.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+//Mission class needs checkpoints and levelObjects
+//also needs a timer displayed to show the elapsed time
+
+namespace SSORF.Management.States
+{
+    //Starting is used for 3..2..1..go and Ending would 
+    //normally be used when all checkpoints are cleared
+
+    public enum MissionState
+    { Starting, Playing, Ending }
+
+    class Mission
+    {
+        public bool Active = true;
+
+        private MissionState state = MissionState.Starting;
+
+        //used for 3..2..1..go
+        TimeSpan countDown = new TimeSpan(0,0,4);
+
+        private Objects.Vehicle scooter;
+        
+        private Model geometry;
+
+        Objects.ThirdPersonCamera camera = new Objects.ThirdPersonCamera();
+
+        //use these fonts to print strings
+        private SpriteFont largeFont;
+        private SpriteFont smallFont;
+
+        //empty constructor for making missions without parameters
+        public Mission()
+        { }
+
+        //when scooter selection menu is implemented we will pass in 
+        //the chosen vehicle to the Mission constructor
+        public Mission(Objects.Vehicle selectedScooter, float aspectRatio)
+        {
+            scooter = selectedScooter;
+            camera.ProjMtx = Matrix.CreatePerspectiveFieldOfView(
+                            MathHelper.ToRadians(45.0f),
+                            aspectRatio, 1.0f, 1000.0f);
+        }
+
+        //missionId can be used to load checkpoint coordinates for that mission
+        //from a file, as well as the filenames/locations of levelObjects, etc.
+        public void load(ContentManager content, short missionID)
+        {
+            //load fonts
+            largeFont = content.Load<SpriteFont>("missionFont");
+            smallFont = content.Load<SpriteFont>("font");
+            //use IDnum to load the correct content
+            geometry = content.Load<Model>("Models\\level" + missionID.ToString());
+            //with IDnum we could also have a different starting positions for each mission
+            scooter.Position = Vector3.Zero;
+            scooter.Yaw = 0.0f;
+        }
+
+        public void update(GameTime gameTime)
+        {
+            //What we update depends on MissionState
+            switch (state)
+            { 
+                //when starting update countdown
+                case MissionState.Starting :
+                    countDown -= gameTime.ElapsedGameTime;
+                    if (countDown.Milliseconds < 0)
+                        state = MissionState.Playing;
+                break;
+
+                //if we are playing update scooter/camera using player input
+                case MissionState.Playing :
+                    scooter.update(gameTime);
+                    camera.update(scooter.Position, scooter.Yaw);
+#if XBOX
+                    if (gamePadState.current.Buttons.Y == ButtonState.Pressed)
+                        state = MissionState.Ending;
+#else
+                    if (keyBoardState.current.IsKeyDown(Keys.E))
+                        state = MissionState.Ending;
+#endif
+                break;
+
+                //If mission has ended wait for user to confirm returning to menu
+                case MissionState.Ending :
+#if XBOX
+                    if (gamePadState.current.Buttons.Start == ButtonState.Pressed)  
+                        Active = false;
+#else
+                    if (keyBoardState.current.IsKeyDown(Keys.Enter))  
+                        Active = false;               
+#endif
+                break;
+            }
+
+        }
+
+        public void draw(SpriteBatch spriteBatch)
+        {
+            // Copy transforms
+            Matrix[] transforms = new Matrix[scooter.Geometry.Bones.Count];
+            geometry.CopyAbsoluteBoneTransformsTo(transforms);
+
+            // Loop through each mesh in model and draw
+            foreach (ModelMesh mesh in geometry.Meshes)
+            {
+                // This is where the mesh orientation is set, as well 
+                // as our camera and projection.
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+
+                    effect.World = transforms[mesh.ParentBone.Index] *
+                            Matrix.CreateFromYawPitchRoll(0.0f, 0.0f, 0.0f) *
+                            Matrix.CreateTranslation(new Vector3(0.0f, 0.0f, 0.0f));
+
+                    effect.View = camera.ViewMtx;
+
+                    effect.Projection = camera.ProjMtx;
+                }
+                // Draw mesh using the effects set in loop above
+                mesh.Draw();
+            }
+
+            scooter.Geometry.CopyAbsoluteBoneTransformsTo(transforms);
+
+            // Loop through each mesh in model and draw
+            foreach (ModelMesh mesh in scooter.Geometry.Meshes)
+            {
+                // This is where the mesh orientation is set, as well 
+                // as our camera and projection.
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+
+                    effect.EnableDefaultLighting();
+
+                    effect.World = transforms[mesh.ParentBone.Index] *
+                            Matrix.CreateFromYawPitchRoll(scooter.Yaw, 0.0f, 0.0f) *
+                            Matrix.CreateTranslation(scooter.Position);
+
+                    effect.View = camera.ViewMtx;
+
+                    effect.Projection = camera.ProjMtx;
+                }
+                // Draw mesh using the effects set in loop above
+                mesh.Draw();
+            }
+
+            //These setting allow us to print strings without screwing with the
+            //3D rendering, but Carl told me it won't work with transparent objects
+            spriteBatch.Begin(SpriteSortMode.FrontToBack, BlendState.AlphaBlend, 
+                SamplerState.AnisotropicClamp, DepthStencilState.Default, 
+                RasterizerState.CullCounterClockwise);
+
+            //display camera and scooter coordinates for testing
+            spriteBatch.DrawString(smallFont, "Scooter coordinates: " + scooter.Position.ToString(), new Vector2(10, 10), Color.Black);
+            spriteBatch.DrawString(smallFont, "Camera coordinates:  " + camera.Position.ToString(), new Vector2(10, 30), Color.Black);
+            
+            //This #if #else will change the on screen instructions when deployed on XBOX
+#if XBOX
+            char endKey = 'Y';
+            string returnKey = "START";
+#else
+            char endKey = 'E';
+            string returnKey = "ENTER";
+#endif
+            //This switch statement prints different instructions depending on MissionState
+            switch (state)
+            { 
+                case MissionState.Starting :
+                    spriteBatch.DrawString(smallFont, "Get ready to race!!!", new Vector2(300, 550), Color.Black);
+                    if (countDown.Seconds > 0)
+                        spriteBatch.DrawString(largeFont, countDown.Seconds.ToString(), new Vector2(340, 100), Color.Black);
+                    else
+                        spriteBatch.DrawString(largeFont, "GO!", new Vector2(220, 100), Color.Black);
+                break;
+
+                case MissionState.Playing :
+                    spriteBatch.DrawString(smallFont, "Press [" + endKey + "] to end mission", new Vector2(280, 550), Color.Black);
+                break;
+
+                case MissionState.Ending :
+                    spriteBatch.DrawString(largeFont, "Finish!", new Vector2(60, 100), Color.Black);
+                    spriteBatch.DrawString(smallFont, "Press [" + returnKey + "] to return to menu", new Vector2(240, 550), Color.Black);
+                break;
+            
+            }
+
+            spriteBatch.End();
+
+        }
+
+        //accessors and mutators
+        public Model Geometry { get { return geometry; } set { geometry = value; } }
+
+        public Objects.ThirdPersonCamera Camera { get { return camera; } 
+            set { camera = value; } }
+    }
+}
