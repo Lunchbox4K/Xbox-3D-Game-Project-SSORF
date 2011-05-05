257c7eb5e57633f30bbd762d51eb0fa12ade80ef
Administrator <Administrator@SNH219-29>
2011-05-05 15:23:35 -0700

    Adding Audio Manager class

---
+++ SSORFwindows/SSORFwindows/Management/States/Mission.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+//Mission class needs checkpoints and levelObjects
+//also needs a timer displayed to show the elapsed time
+
+namespace SSORF.Management.States
+{
+    //Starting is used for 3..2..1..go and Ending would 
+    //normally be used when all checkpoints are cleared
+
+    public enum MissionState
+    { Starting, Playing, Ending, Paused }
+
+    class Mission
+    {
+        public bool Active = true;
+
+        //was the mission completed successfully?
+        private bool missionComplete = false;
+
+        private MissionState state = MissionState.Starting;
+
+        //used for 3..2..1..go
+        TimeSpan countDown = new TimeSpan(0,0,4);
+        TimeSpan timeLimit;
+
+        private Objects.Vehicle scooter;
+
+        private Objects.StaticModel[] CheckPoints;
+
+        private short numCheckPoints = 0;
+
+        private short currentCheckPoint = 0;
+
+        private float checkPointYaw = 0.0f;
+        
+        private Objects.SimpleModel geometry;
+
+        Objects.ThirdPersonCamera camera = new Objects.ThirdPersonCamera();
+
+        //One checkpoint for now...
+        
+
+        //use these fonts to print strings
+        private SpriteFont largeFont;
+        private SpriteFont smallFont;
+
+        //empty constructor for making missions without parameters
+        public Mission()
+        { }
+
+        //when scooter selection menu is implemented we will pass in 
+        //the chosen vehicle to the Mission constructor
+        public Mission(Objects.Vehicle selectedScooter, float aspectRatio)
+        {
+            scooter = selectedScooter;
+            camera.ProjMtx = Matrix.CreatePerspectiveFieldOfView(
+                            MathHelper.ToRadians(45.0f),
+                            aspectRatio, 1.0f, 1000.0f);
+        }
+
+        //missionId can be used to load checkpoint coordinates for that mission
+        //from a file, as well as the filenames/locations of levelObjects, etc.
+        public void load(ContentManager content, short missionID)
+        {
+            //load fonts
+            largeFont = content.Load<SpriteFont>("missionFont");
+            smallFont = content.Load<SpriteFont>("font");
+            //use IDnum to load the correct content
+            geometry = new Objects.SimpleModel();
+            geometry.Mesh = content.Load<Model>("Models\\level" + missionID.ToString());
+
+            //with missionID we can have a different starting positions, checkpoints, etc. for each mission
+            //We need to load the data for each mission from file using the missionID
+            if (missionID == 1)
+            {
+                numCheckPoints = 1;
+                timeLimit = new TimeSpan(0, 0, 4);
+                CheckPoints = new Objects.StaticModel[numCheckPoints];
+
+                for (int i = 0; i < numCheckPoints; i++)
+                {
+                    CheckPoints[i] = new Objects.StaticModel(content, "Models\\check",
+                        Vector3.Zero, Matrix.Identity, Matrix.Identity);
+                    CheckPoints[i].LoadModel();
+                }
+
+                CheckPoints[0].Location = new Vector3(60, 0, 0);
+
+                scooter.setStartingPosition(-0.45f, new Vector3(0, 0, 100));
+            }
+            else if (missionID == 2)
+            {
+                numCheckPoints = 2;
+                timeLimit = new TimeSpan(0, 0, 8);
+                CheckPoints = new Objects.StaticModel[numCheckPoints];
+
+                for (int i = 0; i < numCheckPoints; i++)
+                {
+                    CheckPoints[i] = new Objects.StaticModel(content, "Models\\check",
+                        Vector3.Zero, Matrix.Identity, Matrix.Identity);
+                    CheckPoints[i].LoadModel();
+                }
+
+                CheckPoints[0].Location = new Vector3(0, 0, -80);
+
+                CheckPoints[1].Location = new Vector3(0, 0, 100);
+
+                scooter.setStartingPosition(0.0f, new Vector3(0,0,40));
+            }
+            camera.update(scooter.Geometry.Location, scooter.Yaw);
+        }
+
+
+        public void update(GameTime gameTime)
+        {
+            //What we update depends on MissionState
+            switch (state)
+            { 
+                //when starting update countdown
+                case MissionState.Starting :
+                    countDown -= gameTime.ElapsedGameTime;
+                    if (countDown.Milliseconds < 0)
+                        state = MissionState.Playing;
+
+                    checkPointYaw += 0.05f;
+                    for (int i = currentCheckPoint; i < numCheckPoints; i++)
+                        CheckPoints[i].Orientation = Matrix.CreateRotationY(checkPointYaw);
+
+                break;
+
+                case MissionState.Paused :
+                    //no updates while paused
+#if XBOX
+                    if (gamePadState.current.Buttons.Y == ButtonState.Pressed)
+                        state = MissionState.Ending;
+                    if (gamePadState.current.Buttons.Start == ButtonState.Pressed && 
+                        gamePadState.current.Buttons.Start == ButtonState.Released) 
+                        state = MissionState.Paused;
+#else
+                if (keyBoardState.current.IsKeyDown(Keys.Q))
+                        state = MissionState.Ending;
+                    if (keyBoardState.current.IsKeyDown(Keys.Enter) &&
+                        keyBoardState.previous.IsKeyUp(Keys.Enter))
+                        state = MissionState.Playing;     
+#endif
+                break;
+
+                //if we are playing update scooter/camera using player input
+                case MissionState.Playing :
+                    scooter.update(gameTime);
+                    camera.update(scooter.Geometry.Location, scooter.Yaw);
+
+
+                    timeLimit -= gameTime.ElapsedGameTime;
+                    if (timeLimit.Milliseconds < 0)
+                        state = MissionState.Ending;
+
+                    checkPointYaw += 0.05f;
+                    for (int i = currentCheckPoint; i < numCheckPoints; i++)
+                        CheckPoints[i].Orientation = Matrix.CreateRotationY(checkPointYaw);
+
+                    //Collision Detection
+
+                    //bounding box did not work!
+
+                    
+                    if (CheckPoints[currentCheckPoint].TemporaryCollisionDetection(scooter.Geometry))
+                        currentCheckPoint += 1;
+                    
+
+                    if (currentCheckPoint == numCheckPoints)
+                    { 
+                        missionComplete = true;
+                        state = MissionState.Ending;
+                    }
+
+#if XBOX
+                    if (gamePadState.current.Buttons.Start == ButtonState.Pressed && 
+                        gamePadState.current.Buttons.Start == ButtonState.Released)  
+                        state = MissionState.Paused;
+#else
+
+                    if (keyBoardState.current.IsKeyDown(Keys.Enter) && 
+                        keyBoardState.previous.IsKeyUp(Keys.Enter))
+                        state = MissionState.Paused;     
+#endif
+                break;
+
+
+                //If mission has ended wait for user to confirm returning to menu
+                case MissionState.Ending :
+#if XBOX
+                    if (gamePadState.current.Buttons.Start == ButtonState.Pressed)  
+                        Active = false;
+#else
+                    if (keyBoardState.current.IsKeyDown(Keys.Enter))  
+                        Active = false;               
+#endif
+                break;
+            }
+
+        }
+
+        public void draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            geometry.draw(camera);
+
+            scooter.Geometry.drawModel(gameTime, camera.ViewMtx, camera.ProjMtx);
+
+            for (int i = currentCheckPoint; i < numCheckPoints; i++)
+                CheckPoints[i].drawModel(gameTime, camera.ViewMtx, camera.ProjMtx);
+
+            
+            //These setting allow us to print strings without screwing with the
+            //3D rendering, but Carl told me it won't work with transparent objects
+            spriteBatch.Begin(SpriteSortMode.FrontToBack, BlendState.AlphaBlend, 
+                SamplerState.AnisotropicClamp, DepthStencilState.Default, 
+                RasterizerState.CullCounterClockwise);
+
+            //display camera and scooter coordinates for testing
+            //spriteBatch.DrawString(smallFont, "Scooter coordinates: " + scooter.Geometry.Location.ToString(), new Vector2(10, 10), Color.Black);
+            //spriteBatch.DrawString(smallFont, "Camera coordinates:  " + camera.Position.ToString(), new Vector2(10, 30), Color.Black);
+            
+            //This #if #else will change the on screen instructions when deployed on XBOX
+#if XBOX
+            char endKey = 'Y';
+            string returnKey = "START";
+#else
+            char endKey = 'Q';
+            string returnKey = "ENTER";
+#endif
+            //This switch statement prints different instructions depending on MissionState
+            switch (state)
+            { 
+                case MissionState.Starting :
+                    spriteBatch.DrawString(smallFont, "Time Left: " + timeLimit.TotalSeconds.ToString(), new Vector2(200, 10), Color.Black);
+                    spriteBatch.DrawString(smallFont, "Get ready to race!!!", new Vector2(300, 550), Color.Black);
+                    if (countDown.Seconds > 0)
+                        spriteBatch.DrawString(largeFont, countDown.Seconds.ToString(), new Vector2(340, 100), Color.Black);
+                    else
+                        spriteBatch.DrawString(largeFont, "GO!", new Vector2(220, 100), Color.Black);
+                break;
+
+                case MissionState.Paused:
+                    spriteBatch.DrawString(smallFont, "Time Left: " + timeLimit.TotalSeconds.ToString(), new Vector2(200, 10), Color.Black);
+                    spriteBatch.DrawString(largeFont, "paused", new Vector2(80, 100), Color.Black);
+                    spriteBatch.DrawString(smallFont, "Press [" + endKey + "] to quit mission", new Vector2(280, 530), Color.Black);
+                    spriteBatch.DrawString(smallFont, "Press [" + returnKey + "] to return to mission", new Vector2(280, 550), Color.Black);
+                break;
+
+                case MissionState.Playing :
+                    spriteBatch.DrawString(smallFont, "Time Left: " + timeLimit.TotalSeconds.ToString(), new Vector2(200, 10), Color.Black);
+                    spriteBatch.DrawString(smallFont, "Press [" + returnKey + "] to pause mission", new Vector2(280, 550), Color.Black);
+                break;
+
+                case MissionState.Ending :
+                if (missionComplete)
+                {
+                    spriteBatch.DrawString(largeFont, "Finish!", new Vector2(60, 100), Color.Black);
+                    spriteBatch.DrawString(smallFont, "With " + timeLimit.TotalSeconds.ToString() + " seconds to spare!!!", new Vector2(200, 450), Color.Black);
+                }
+                else
+                    spriteBatch.DrawString(largeFont, "Fail!", new Vector2(200, 100), Color.Black);
+
+                    spriteBatch.DrawString(smallFont, "Press [" + returnKey + "] to return to menu", new Vector2(240, 550), Color.Black);
+                break;
+            
+            }
+
+            spriteBatch.End();
+
+        }
+
+        //accessors and mutators
+        public Objects.SimpleModel Geometry { get { return geometry; } set { geometry = value; } }
+
+        public Objects.ThirdPersonCamera Camera { get { return camera; } 
+            set { camera = value; } }
+    }
+}
---
+++ SSORFwindows/SSORFwindows/Objects/Vehicle.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+//Contains 3D model and specs for a scooter
+namespace SSORF.Objects
+{
+    public class Vehicle
+    {
+        StaticModel geometry;
+        float speed = 1.4f;
+        float yaw;
+        float wheelAngle = 0;
+        float wheelMaxAngle = (float).785;
+        int wheelBaseLength = 24;
+        //vehicle still needs list of specs such as weight, name, etc
+        //Also need a way to add upgrades to vehicles
+
+        public void load(ContentManager content, short vehicleID)
+        {
+            geometry = new StaticModel(content, "Models\\scooter" + vehicleID.ToString(),
+                Vector3.Zero, Matrix.Identity, Matrix.Identity);
+            geometry.LoadModel();
+
+            //Things to load here:
+            //value of wheelMaxAngle - DO IT IN RADIANS
+            //value of wheelBaseLength
+            //other vehicle specs
+            //upgrade specs
+        }
+
+        public void setStartingPosition(float startingYaw, Vector3 startingPosition)
+        {
+            yaw = startingYaw;
+            geometry.Orientation = Matrix.CreateRotationY(yaw);
+            geometry.Location = startingPosition;
+        }
+
+        public void update(GameTime gameTime)
+        {
+            //TODO: calculate velocity from accel/decel
+            //Get the integral of the vehicle's velocity
+            float tempDistance = speed *gamePadState.current.Triggers.Right;
+            //Find the vehicle's current turning radius
+            float turnRadius = wheelBaseLength/(float)Math.Sin(wheelAngle);
+            //Now use those to get the vehicle's yaw offset - i love radians
+            float tempYaw = tempDistance / turnRadius;
+
+            //Update rotations
+            yaw += tempYaw;
+
+            geometry.Orientation = Matrix.CreateRotationY(yaw);
+
+            //Derive and update position
+            geometry.Location += geometry.Orientation.Forward * tempDistance * (float)Math.Cos(tempYaw);
+            geometry.Location += geometry.Orientation.Left * tempDistance * (float)Math.Sin(tempYaw);
+
+            //@TODO: calculate velocity by end of frame
+            //Capture the wheel angle for the next frame's worth of motion
+            wheelAngle = gamePadState.current.ThumbSticks.Left.X * wheelMaxAngle;
+
+            //KeyBoard input
+            //NOTE: Don't delete this region!! I need to move to test collisions with checkpoints!!!
+            #region
+            if (keyBoardState.current.IsKeyDown(Keys.Up))
+               geometry.Location += geometry.Orientation.Forward * 1.5f;
+            if (keyBoardState.current.IsKeyDown(Keys.Left))
+                yaw += 0.1f;
+            if (keyBoardState.current.IsKeyDown(Keys.Right))
+                yaw -= 0.1f;
+
+            geometry.Orientation = Matrix.CreateRotationY(yaw);
+            #endregion
+        }
+ 
+        //Accessors and Mutators
+        public StaticModel Geometry { get { return geometry; } set { geometry = value; } }
+
+        public float Yaw { get { return yaw; } set { yaw = value; } }
+
+    }
+}
