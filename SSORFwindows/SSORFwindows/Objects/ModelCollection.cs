e0ffd2a9ffc9fb906a903a4818ab20e9e9b984f1
Sidareus <Sidareus@localhost>
2011-05-10 16:13:21 -0700

    Merge

---
+++ SSORFwindows/SSORFwindows/Objects/ModelCollection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SSORF.Objects
+{
+    class ModelCollection
+    {
+        private StaticModel geometry;
+        private Vector3[] coordinates;
+        private short numModels;
+
+        public ModelCollection(StaticModel model, short numClones, Vector3[] locations)
+        {
+            coordinates = new Vector3[numClones];
+            coordinates = locations;
+            geometry = model;
+            numModels = numClones;
+        }
+
+        public bool CheckCollision(short whichModel, StaticModel otherModel)
+        {
+            geometry.Location = coordinates[whichModel];
+            return geometry.TemporaryCollisionDetection(otherModel);
+        }
+
+        public void draw(GameTime gameTime, ThirdPersonCamera camera)
+        { 
+            for(int i = 0; i < numModels; i++)
+            {
+                geometry.Location = coordinates[i];
+                geometry.drawModel(gameTime, camera.ViewMtx, camera.ProjMtx);
+            }
+        
+        }
+
+        public void draw(GameTime gameTime, ThirdPersonCamera camera, short start, short end)
+        {
+            if (end >= numModels)
+                end = (short)(numModels - 1);
+            for (int i = start; i <= end; i++)
+            {
+                geometry.Location = coordinates[i];
+                geometry.drawModel(gameTime, camera.ViewMtx, camera.ProjMtx);
+            }
+        }
+
+        public StaticModel Geometry
+        {
+            get { return geometry; }
+            set{ geometry = value; }
+        }
+    }
+}
---
+++ SSORFwindows/SSORFwindows/Objects/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//This class needs an array of scooters and the ability to add scooters to the players
+//list of purchased scooters or buy upgrades if they have enough money.
+namespace SSORF.Objects
+{
+    
+    class Player
+    {
+
+        //private int money = 0;
+
+        //An array of the ID numbers for scooters owned
+        private short[] scootersOwned = new short[10] {0,0,0,0,0,0,0,0,0,0};
+        //Id number of the selected scooter
+        private short selectedScooter = 1;
+
+        public short SelectedScooter
+        {
+            get { return selectedScooter; }
+            set { selectedScooter = value; } 
+        }
+
+    }
+}
