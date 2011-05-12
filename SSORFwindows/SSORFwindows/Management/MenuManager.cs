36d306301dfce3275e02deae7d7a11da36cecfd1
Administrator <Administrator@SNH219-24.acs.edcc.ctc.edu>
2011-05-12 14:17:23 -0700

    BCbutton.png FCbutton.png messagebox.png POSbutton.png TuneShopButton.png TuneShopTest.png UpgradeData.cs  MessageBox.cs upgrades.xml

--- SSORFwindows/SSORFwindows/Management/MenuManager.cs
+++ SSORFwindows/SSORFwindows/Management/MenuManager.cs
@@ -8,6 +8,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using SSORFlibrary;
 
 //MenuManager will need functionality added to allow player to go 
 //back and forth between different menus by setting the currentMenu
@@ -19,15 +20,19 @@ namespace SSORF.Management
     {
 
         //Which menu is currently being viewed?
-        enum Menu
+        enum Menu : int
         {
-            Main,
+            Main = 0,
             VehicleSelect,
             Dealership,
             Missions,
+            TuneShop,
             RaceState,
             NumMenus,
         }
+
+        Objects.MessageBox messageBox = new Objects.MessageBox();
+
         Menu CurrentMenu;
         //Needs a lot more submenus.
         States.SubMenu[] Menus;
@@ -40,10 +45,21 @@ namespace SSORF.Management
         //to a mission. If no mission is selected, menu is active. 
         public short selectedMission = 0;
        
+        //used to display upgrade data
+        UpgradeData[] upgrades;
+
+        SpriteFont menuFont;
+
         public MenuManager(ContentManager content)
         {
+            //Load UpgradeData
+            upgrades = content.Load<UpgradeData[]>("upgrades");
+            //font for displaying upgrade data
+            menuFont = content.Load<SpriteFont>("menuFont");
+
             Menus = new States.SubMenu[(int)Menu.NumMenus];
-            //Load Main
+
+            #region Load Main Menu
             Menus[(int)Menu.Main] = new States.SubMenu(7); //mission menu has 2 buttons
             Menus[(int)Menu.Main].BackGround = content.Load<Texture2D>("Images\\game menu");
             Menus[(int)Menu.Main].ButtonImage[0] = content.Load<Texture2D>("Images\\Missions");
@@ -60,10 +76,9 @@ namespace SSORF.Management
             Menus[(int)Menu.Main].ButtonPosition[5] = new Vector2(35, 390);
             Menus[(int)Menu.Main].ButtonImage[6] = content.Load<Texture2D>("Images\\TestButton");
             Menus[(int)Menu.Main].ButtonPosition[6] = new Vector2(35, 440);
+            #endregion
 
-            //Vehicleselect
-           
-            
+            #region Load VehicleSelect
             Menus[(int)Menu.VehicleSelect] = new States.SubMenu(3); //mission menu has 2 buttons
             Menus[(int)Menu.VehicleSelect].BackGround = content.Load<Texture2D>("Images\\VehicleTest");
             Menus[(int)Menu.VehicleSelect].ButtonImage[0] = content.Load<Texture2D>("Images\\vehicle1");
@@ -72,18 +87,32 @@ namespace SSORF.Management
             Menus[(int)Menu.VehicleSelect].ButtonPosition[1] = new Vector2(450, 450);
             Menus[(int)Menu.VehicleSelect].ButtonImage[2] = content.Load<Texture2D>("Images\\BackButton");
             Menus[(int)Menu.VehicleSelect].ButtonPosition[2] = new Vector2(50, 70);
+            #endregion
 
-            //Dealership
+            #region Load Dealership
             Menus[(int)Menu.Dealership] = new States.SubMenu(3); //mission menu has 2 buttons
             Menus[(int)Menu.Dealership].BackGround = content.Load<Texture2D>("Images\\VehicleTest");
             Menus[(int)Menu.Dealership].ButtonImage[0] = content.Load<Texture2D>("Images\\TestButton");
             Menus[(int)Menu.Dealership].ButtonPosition[0] = new Vector2(100, 450);
-            Menus[(int)Menu.Dealership].ButtonImage[1] = content.Load<Texture2D>("Images\\TestButton");
-            Menus[(int)Menu.Dealership].ButtonPosition[1] = new Vector2(450, 450);
+            Menus[(int)Menu.Dealership].ButtonImage[1] = content.Load<Texture2D>("Images\\TuneShopButton");
+            Menus[(int)Menu.Dealership].ButtonPosition[1] = new Vector2(670, 50);
             Menus[(int)Menu.Dealership].ButtonImage[2] = content.Load<Texture2D>("Images\\BackButton");
             Menus[(int)Menu.Dealership].ButtonPosition[2] = new Vector2(50, 70);
+            #endregion
 
-            //load missionsMenu stuff
+            #region Load TuneShop
+            Menus[(int)Menu.TuneShop] = new States.SubMenu(4); //tune shop has 4 buttons
+            Menus[(int)Menu.TuneShop].BackGround = content.Load<Texture2D>("Images\\TuneShopTest");
+            for (int i = 0; i < 3; i++)
+                Menus[(int)Menu.TuneShop].ButtonImage[i] = content.Load<Texture2D>("Images\\" + upgrades[i].button);    
+            Menus[(int)Menu.TuneShop].ButtonPosition[0] = new Vector2(150, 350);
+            Menus[(int)Menu.TuneShop].ButtonPosition[1] = new Vector2(250, 350);
+            Menus[(int)Menu.TuneShop].ButtonPosition[2] = new Vector2(350, 350);
+            Menus[(int)Menu.TuneShop].ButtonImage[3] = content.Load<Texture2D>("Images\\BackButton");
+            Menus[(int)Menu.TuneShop].ButtonPosition[3] = new Vector2(50, 70);
+            #endregion
+
+            #region Load MissionsMenu
             Menus[(int)Menu.Missions] = new States.SubMenu(3); //mission menu has 2 buttons
             Menus[(int)Menu.Missions].BackGround = content.Load<Texture2D>("Images\\menu");
             Menus[(int)Menu.Missions].ButtonImage[0] = content.Load<Texture2D>("Images\\button1");
@@ -92,33 +121,37 @@ namespace SSORF.Management
             Menus[(int)Menu.Missions].ButtonPosition[1] = new Vector2(450, 300);
             Menus[(int)Menu.Missions].ButtonImage[2] = content.Load<Texture2D>("Images\\BackButton");
             Menus[(int)Menu.Missions].ButtonPosition[2] = new Vector2(600, 550);
-            
-            //load cursor image and set current menu to missions menu
-            CursorImage = content.Load<Texture2D>("Images\\cursor");
+            #endregion
 
-            //This should be changed to main menu once we have it working
+            //load messageBox background
+            messageBox.Background = content.Load<Texture2D>("Images\\messagebox");
+
+            //load cursor image and set current menu to main menu
+            CursorImage = content.Load<Texture2D>("Images\\cursor");
             CurrentMenu = Menu.Main;
-            
+
+
+
         }
 
         public void update(GameTime gameTime, Objects.Player player)
         {
+            if (!messageBox.Active)
+            {
 
-            Menus[(int)CurrentMenu].update(gameTime);
+                Menus[(int)CurrentMenu].update(gameTime);
 
-            //Here we could add logic to switch current menu if the back button is pressed, or return to main menu
-            switch (CurrentMenu)
-            { 
+                switch (CurrentMenu)
+                {
 
-                case Menu.Main :
-                   
+                    case Menu.Main:
 
                         if (Menus[(int)Menu.Main].buttonPressed == 1)
                             CurrentMenu = Menu.Missions;
                         else if (Menus[(int)Menu.Main].buttonPressed == 2)
-                            CurrentMenu = Menu.VehicleSelect;
-                        else if (Menus[(int)Menu.Main].buttonPressed == 3)
                             CurrentMenu = Menu.Dealership;
+                        else if (Menus[(int)Menu.Main].buttonPressed == 3)
+                            CurrentMenu = Menu.VehicleSelect;
                         //else if (Menus[(int)Menu.Main].buttonPressed == 4)
                         //    CurrentMenu = Menu.VehicleSelect;
                         //else if (Menus[(int)Menu.Main].buttonPressed == 5)
@@ -128,57 +161,128 @@ namespace SSORF.Management
                         else if (Menus[(int)Menu.Main].buttonPressed == 7)
                             CurrentMenu = Menu.RaceState;
                         Menus[(int)Menu.Main].buttonPressed = 0;
-                    
-                    break;
 
-                case Menu.Missions :
+                        break;
 
-                    //Note: buttonPressed = 0 means no button has been pressed
-                    //If we are in the missions menu and a button is pressed...
-                    if (Menus[(int)Menu.Missions].buttonPressed == 3)
-                        CurrentMenu = Menu.Main;
-                    else if (Menus[(int)Menu.Missions].buttonPressed != 0)
-                    {
-                        //Set selected mission to a value other than zero to deactivate the menu
-                        selectedMission = Menus[(int)Menu.Missions].buttonPressed;
-                        //reset buttonPressed for when we return to the menu
-                       
-                    }
-                    Menus[(int)Menu.Missions].buttonPressed = 0;
-                    
-                    break;
+                    case Menu.Missions:
 
-                case Menu.Dealership :
-                    if (Menus[(int)Menu.Dealership].buttonPressed == 3)
-                        CurrentMenu = Menu.Main;
-                    Menus[(int)Menu.Dealership].buttonPressed = 0;
-                    break;
+                        //Note: buttonPressed = 0 means no button has been pressed
+                        //If we are in the missions menu and a button is pressed...
+                        if (Menus[(int)Menu.Missions].buttonPressed == 3)
+                            CurrentMenu = Menu.Main;
+                        else if (Menus[(int)Menu.Missions].buttonPressed != 0)
+                        {
+                            //Set selected mission to a value other than zero to deactivate the menu
+                            selectedMission = Menus[(int)Menu.Missions].buttonPressed;
+                            //reset buttonPressed for when we return to the menu
 
-                case Menu.VehicleSelect :
-                    if (Menus[(int)Menu.VehicleSelect].buttonPressed == 3)
-                        CurrentMenu = Menu.Main;
-                    if (Menus[(int)Menu.VehicleSelect].buttonPressed > 0 &&
-                        Menus[(int)Menu.VehicleSelect].buttonPressed < 3)
-                    {
-                        player.SelectedScooter = Menus[(int)Menu.VehicleSelect].buttonPressed;
-                        CurrentMenu = Menu.Main;
-                    }
-                    Menus[(int)Menu.VehicleSelect].buttonPressed = 0;
-                    break;
+                        }
+                        Menus[(int)Menu.Missions].buttonPressed = 0;
+
+                        break;
+
+                    case Menu.Dealership:
+                        if (Menus[(int)Menu.Dealership].buttonPressed == 3)
+                            CurrentMenu = Menu.Main;
+                        if (Menus[(int)Menu.Dealership].buttonPressed == 2)
+                            CurrentMenu = Menu.TuneShop;
+
+                        Menus[(int)Menu.Dealership].buttonPressed = 0;
+                        break;
+
+                    case Menu.TuneShop:
+                        if (Menus[(int)Menu.TuneShop].buttonPressed == 4)
+                            CurrentMenu = Menu.Dealership;
+
+                        if (Menus[(int)Menu.TuneShop].buttonPressed > 0 &&
+                            Menus[(int)Menu.TuneShop].buttonPressed < 4)
+                        {
+                            string TuneShopMessage = player.PurchaseUpgrade(
+                            upgrades[Menus[(int)Menu.TuneShop].buttonPressed - 1]);
+                            messageBox.setMessage("Message", TuneShopMessage);
+                            messageBox.Active = true;
+                        }
+
+                        Menus[(int)Menu.TuneShop].buttonPressed = 0;
+                        break;
+
+                    case Menu.VehicleSelect:
+                        if (Menus[(int)Menu.VehicleSelect].buttonPressed == 3)
+                            CurrentMenu = Menu.Main;
+                        if (Menus[(int)Menu.VehicleSelect].buttonPressed > 0 &&
+                            Menus[(int)Menu.VehicleSelect].buttonPressed < 3)
+                        {
+                            player.SelectedScooter = Menus[(int)Menu.VehicleSelect].buttonPressed;
+                            CurrentMenu = Menu.Main;
+                        }
+                        Menus[(int)Menu.VehicleSelect].buttonPressed = 0;
+                        break;
 
                     //etc.
-            }
-            
-            
-        }
+                }
 
+
+            }
+            else
+                messageBox.update();
+
+        }
         //draw the current menu and the cursor
-        public void draw(SpriteBatch spriteBatch)
+        public void draw(SpriteBatch spriteBatch, Objects.Player player)
         {
             spriteBatch.Begin();
             Menus[(int)CurrentMenu].draw(spriteBatch);
+
+            
+            switch (CurrentMenu)
+            {
+                //display upgrade data
+                case Menu.TuneShop :
+
+                    spriteBatch.DrawString(menuFont, "You have $" + player.Money.ToString(),
+                        new Vector2(50,550), Color.Tan);
+
+                    if (Menus[(int)Menu.TuneShop].SelectedButton != 4)
+                    {
+                        drawUpgradeSpecs(spriteBatch);
+                    }
+                    break;
+            }
+
             spriteBatch.Draw(CursorImage, Menus[(int)CurrentMenu].CursorPosition, Color.White);
+
+            if (messageBox.Active)
+                messageBox.draw(spriteBatch, menuFont, Color.SlateGray, Color.Black);
+
             spriteBatch.End();
+        }
+
+        public void drawUpgradeSpecs(SpriteBatch spriteBatch)
+        {
+
+            spriteBatch.DrawString(menuFont,
+                upgrades[Menus[(int)Menu.TuneShop].SelectedButton - 1].name,
+                new Vector2(200, 100), Color.Tan);
+
+            spriteBatch.DrawString(menuFont, 
+                upgrades[Menus[(int)Menu.TuneShop].SelectedButton - 1].description1, 
+                new Vector2(200, 140), Color.Tan);
+
+            spriteBatch.DrawString(menuFont, 
+                upgrades[Menus[(int)Menu.TuneShop].SelectedButton - 1].description2, 
+                new Vector2(200, 160), Color.Tan);
+
+            spriteBatch.DrawString(menuFont, 
+                "Power:  " + upgrades[Menus[(int)Menu.TuneShop].SelectedButton - 1].power.ToString(), 
+                new Vector2(200, 200), Color.Tan);
+
+            spriteBatch.DrawString(menuFont, 
+                "Weight:  " + upgrades[Menus[(int)Menu.TuneShop].SelectedButton - 1].weight.ToString(), 
+                new Vector2(200, 220), Color.Tan);
+
+            spriteBatch.DrawString(menuFont, 
+                "Cost:  $" + upgrades[Menus[(int)Menu.TuneShop].SelectedButton - 1].cost.ToString(), 
+                new Vector2(200, 240), Color.Tan);
         }
 
     }
--- SSORFwindows/SSORFwindows/Objects/Player.cs
+++ SSORFwindows/SSORFwindows/Objects/Player.cs
@@ -7,14 +7,26 @@ using System.Text;
 //list of purchased scooters or buy upgrades if they have enough money.
 namespace SSORF.Objects
 {
-    
+     struct upgradeSpecs
+        {
+            public float power;
+            public float weight;
+        }
+
     class Player
     {
+       
 
-        //private int money = 0;
+        private int money = 1000;
 
         //An array of the ID numbers for scooters owned
-        private short[] scootersOwned = new short[10] {0,0,0,0,0,0,0,0,0,0};
+        private short[] scootersOwned = new short[10] {1,2,0,0,0,0,0,0,0,0};
+
+        private bool[,] upgradesPurchased = new bool[10,3];
+
+        //An array of stat changes corresponding to 
+        private upgradeSpecs[] scooterUpgrades = new upgradeSpecs[10];
+
         //Id number of the selected scooter
         private short selectedScooter = 1;
 
@@ -24,5 +36,35 @@ namespace SSORF.Objects
             set { selectedScooter = value; } 
         }
 
+        public int Money
+        {
+            get { return money; }
+        }
+
+        private bool PurchaseItem(short cost)
+        {
+            if (cost > money)
+                return false;
+            else
+                money -= cost;
+
+            return true;
+        }
+
+        public string PurchaseUpgrade(SSORFlibrary.UpgradeData upgrade)
+        {
+            //if upgrade already purchased for that scooter return false
+            if (upgradesPurchased[selectedScooter,upgrade.IDnum] == true)
+                return "This upgrade has already been purchased";
+            //if player has insufficent funds return false
+            if (PurchaseItem(upgrade.cost) == false)
+                return "You don't have enough cash for this upgrade";
+
+            //otherwise add upgrade values to total upgrade specs for that scooter
+            scooterUpgrades[selectedScooter].power += upgrade.power;
+            scooterUpgrades[selectedScooter].weight += upgrade.weight;
+            upgradesPurchased[selectedScooter, upgrade.IDnum] = true;
+            return "Upgrade purchased";
+        }
     }
 }
