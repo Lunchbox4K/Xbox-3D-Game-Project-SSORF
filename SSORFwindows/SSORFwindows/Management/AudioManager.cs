548682399794c8a5946b5e95ae04e786826b0775
Administrator <Administrator@SNH219-16.acs.edcc.ctc.edu>
2011-05-19 16:42:27 -0700

    Fixed aerodynamic modeling & compensated for rolling/drivetrain resistance

---
+++ SSORFwindows/SSORFwindows/Management/AudioManager.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using System;
+
+namespace SSORF.Management
+{
+    public static class AudioManager
+    {
+        private static AudioEngine audioEngine;
+        private static WaveBank waveBank;
+        private static SoundBank soundBank;
+
+        private static Cue menuMusic;
+        private static Cue missionMusic;
+        private static Cue engineSounds;
+
+        public static void LoadAudioContent()
+        {
+            audioEngine = new AudioEngine("Content/Audio/Background Music.xgs");
+            waveBank = new WaveBank(audioEngine, "Content/Audio/Wave Bank.xwb");
+            soundBank = new SoundBank(audioEngine, "Content/Audio/Sound Bank.xsb");
+
+            menuMusic = soundBank.GetCue("Exciting Ride");
+            missionMusic = soundBank.GetCue("Journey");
+            engineSounds = soundBank.GetCue("Engine");
+        }
+
+        public static void Update()
+        {
+            audioEngine.Update();
+        }
+
+        public static Cue getCue(String cueName)
+        {
+            return soundBank.GetCue(cueName);
+        }
+
+        public static Cue getMenuMusic()
+        {
+            return menuMusic;
+        }
+
+        public static void resetMenuMusic()
+        {
+            menuMusic = soundBank.GetCue("Exciting Ride");
+        }
+
+        public static Cue getMissionMusic()
+        {
+            return missionMusic;
+        }
+
+        public static void resetMissionMusic()
+        {
+            missionMusic = soundBank.GetCue("Journey");
+        }
+
+        public static Cue getEngineSounds()
+        {
+            return engineSounds;
+        }
+
+        public static void resetEngineSounds()
+        {
+            engineSounds = soundBank.GetCue("At Speed");
+        }
+
+        public static void setEngineSounds(String cueName)
+        {
+            engineSounds = getCue(cueName);
+        }
+    }
+}
---
+++ SSORFwindows/SSORFwindows/Objects/Player.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//This class needs an array of scooters and the ability to add scooters to the players
+//list of purchased scooters or buy upgrades if they have enough money.
+namespace SSORF.Objects
+{
+     public struct upgradeSpecs
+        {
+            public float power;
+            public float weight;
+        }
+
+    class Player
+    {
+        private int money = 15000;
+
+        //indexes of bool array correspond to scooter ID nums
+        private bool[] scootersOwned = new bool[10] 
+        {true,false,false,false,false,false,false,false,false,false};
+        //Player starts with amigo RTX (ID = 0)
+
+        private bool[,] upgradesPurchased = new bool[10,3];
+
+        //An array of stat changes corresponding to 
+        private upgradeSpecs[] scooterUpgrades = new upgradeSpecs[10];
+
+        //Id number of the selected scooter
+        private short selectedScooter = 0;
+
+        private bool PurchaseItem(short cost)
+        {
+            if (cost > money)
+                return false;
+            else
+                money -= cost;
+
+            return true;
+        }
+
+        public string PurchaseUpgrade(SSORFlibrary.UpgradeData upgrade)
+        {
+            //if upgrade already purchased for that scooter return message
+            if (upgradesPurchased[selectedScooter,upgrade.IDnum] == true)
+                return "This upgrade has already been purchased";
+            //if player has insufficent funds return message
+            if (PurchaseItem(upgrade.cost) == false)
+                return "You don't have enough cash for this upgrade";
+
+            //otherwise add upgrade values to total upgrade specs for that scooter
+            scooterUpgrades[selectedScooter].power += upgrade.power;
+            scooterUpgrades[selectedScooter].weight += upgrade.weight;
+            upgradesPurchased[selectedScooter, upgrade.IDnum] = true;
+            return "Upgrade purchased";
+        }
+
+        public string PurchaseScooter(SSORFlibrary.ScooterData scooter)
+        {
+            //if scooter is already owned return message
+            if (scootersOwned[scooter.IDnum] == true)
+                return "You already own this scooter";
+            //if player has insufficent funds return message
+            if (PurchaseItem(scooter.cost) == false)
+                return "You don't have enough cash to buy this scooter";
+
+            scootersOwned[scooter.IDnum] = true;
+            return scooter.name + " was delivered to your garage";
+        }
+
+
+        public short SelectedScooter
+        {
+            get { return selectedScooter; }
+            set { selectedScooter = value; }
+        }
+
+        public int Money
+        {
+            get { return money; }
+            set { money = value; }
+        }
+
+        public bool[] ScootersOwned
+        {
+            get { return scootersOwned; }
+        }
+
+        public upgradeSpecs[] UpgradeTotals
+        {
+            get { return scooterUpgrades; }
+        }
+    }
+}
