25a075f19522c385629b0acfcd4e355989c34dab
Brian <Brian@localhost>
2011-05-24 15:54:07 -0700

    Updated AudioManager.cs to include UpdateMusic() and music/sound playing booleans.

--- SSORFwindows/SSORFwindows/Management/AudioManager.cs
+++ SSORFwindows/SSORFwindows/Management/AudioManager.cs
@@ -27,6 +27,52 @@ namespace SSORF.Management
             engineSounds = soundBank.GetCue("Engine");
         }
 
+        public static void UpdateMusic(GameState state)
+        {
+            switch (state)
+            {
+                // If we are viewing title screen...
+                case GameState.TitleScreen:
+                    break;
+
+                // If we are viewing the menus...
+                case GameState.MenuScreen:
+                    //Only play the music if music is on
+                    if (isMusicPlaying)
+                    {
+                        if (!menuMusic.IsPlaying && menuMusic.IsPrepared)
+                            menuMusic.Play();
+                        else if (menuMusic.IsStopped)
+                        {
+                            resetMenuMusic();
+                            menuMusic.Play();
+                        }
+                    }
+                    if (missionMusic.IsPlaying)
+                        missionMusic.Stop(AudioStopOptions.AsAuthored);
+                    if (engineSounds.IsPlaying)
+                        engineSounds.Stop(AudioStopOptions.AsAuthored);
+                    break;
+
+                // If we are playing a mission...
+                case GameState.MissionScreen:
+                    //Only play the music if music is on
+                    if (isMusicPlaying)
+                    {
+                        if (!missionMusic.IsPlaying && missionMusic.IsPrepared)
+                            missionMusic.Play();
+                        else if (missionMusic.IsStopped)
+                        {
+                            resetMissionMusic();
+                            missionMusic.Play();
+                        }
+                    }
+                    if (menuMusic.IsPlaying)
+                        menuMusic.Stop(AudioStopOptions.AsAuthored);
+                    break;
+            }
+        }
+
         public static void Update()
         {
             audioEngine.Update();
@@ -70,6 +116,16 @@ namespace SSORF.Management
         public static void setEngineSounds(String cueName)
         {
             engineSounds = getCue(cueName);
+        }
+
+        public static void setMusicPlaying(Boolean isPlaying)
+        {
+            isMusicPlaying = isPlaying;
+        }
+
+        public static void setSoundPlaying(Boolean isPlaying)
+        {
+            isSoundPlaying = isPlaying;
         }
     }
 }
